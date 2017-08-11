c43e5246a7a351567c5751ca7009a50fb0b6da8e
SKapal <sahilkapal@cmail.carleton.ca>
2017-08-11 13:23:31 -0400

    Method and attributes needed commented in

---
+++ MindTheEights.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class MindTheEights {
+
+    //    should always be aware of any eights they are holding. This player will their eights until late in the game,
+    //    but won't hold on to them too long (as they are worth a lot of points). Once any opponent goes down to one card,
+    //    it's time to play your eight. If you have two eights, start playing them once an opponent goes down to two cards.
+    //    (Expand this for 3 or 4 or more eights.)
+
+    //    Methods: Player discard: (if drawpile.match), player draw: (if not matching),
+    //             place eight: tracks num of 8s = n, player discard calls if any player has n cards left
+    //    Attributes:
+    //    Constructor: public MindTheEights(Card[] cards)
+
+}
